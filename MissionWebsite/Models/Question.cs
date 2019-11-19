a7414285e7b872af63dd2727a08b2aff7e6f399b
Logan Sackley <logansackley10@gmail.com>
2019-11-19 15:29:12 -0700

    update

---
+++ MissionWebsite/Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MissionWebsite.Models
+{
+    public class Question
+    {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        public int QuestionID { get; set; }
+
+        [Required(ErrorMessage = "You must type a message to submit")]
+        [DisplayName("Question")]
+        [StringLength(500)]
+        public string sMessage { get; set; }
+    }
+}
